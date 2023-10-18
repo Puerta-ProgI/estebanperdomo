3fa342265174ea38ded87689fede03ddbab56599
Programación - Puerta <147578036+Puerta-ProgI@users.noreply.github.com>
2023-10-18 14:44:20 -0300

    Add files via upload

---
+++ 1ro.js
@@ -0,0 +1,55 @@
+/*window.addEventListener("load", async function () {
+/* console.log("hola");
+  let termo = {
+    litros: 2,
+    color: "negro",
+  };
+  let ul = document.querySelector("ul");
+  ul.innerHTML += `<li class="asd">Termo Charly</li>`;
+  for (i = 0; i < 4; i++) {
+    ul.innerHTML += `<li class="asd">Termo Charly</li>`;
+  }
+  let usuarios = [
+    {
+      nombre: "Pepe",
+      apellido: "Argento",
+      correo: "usuariouno@gmail.com",
+    },
+    {
+      nombre: "Ricardo",
+      apellido: "Fort",
+      correo: "usuariodos@gmail.com",
+    },
+    {
+      nombre: "Carlitos",
+      apellido: "Sinche",
+      correo: "usuariotres@gmail.com",
+    },
+  ];
+
+  usuarios.forEach(function (Usuario) {
+    ul.innerHTML += `<div class="card" style="width: 18rem">
+  <img src="..." class="card-img-top" alt="..." />
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">
+      Some quick example text to build on the card title and make up the bulk
+      of the card's content.
+    </p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>`;
+  });
+  ul.classList.toggle("dark");
+  async function duplicar(numero) {
+    return numero * 2;
+  }
+  async function imprimir(valor) {
+    console.log(await valor);
+  }
+  imprimir(duplicar(2));
+  let resultado = await fetch("https://jsonplaceholder.typicode.com/users");
+  let data = await resultado.json();
+  console.log(data);
+});
+ */
---
+++ array.js
@@ -0,0 +1,93 @@
+let colores = [
+    {
+        nombre:"Rojo",
+        codigo:"#FF0000"
+    },
+    {
+        nombre:"Verde",
+        codigo:"#00FF00"
+    },
+    {
+        nombre:"Azul",
+        codigo:"#0000FF"
+    }
+];
+
+let usuarios = [
+    {
+        nombre: "Usuario",
+        apellido: "Uno",
+        correo: "usuariouno@gmail.com"
+    },
+    {
+        nombre: "Usuario",
+        apellido: "Dos",
+        correo: "usuariodos@gmail.com"
+    },
+    {
+        nombre: "Usuario",
+        apellido: "Tres",
+        correo: "usuariotres@gmail.com"
+    }
+]
+
+let productos = [
+    {
+        nombre: "Producto 1",
+        descripcion: "Descripción 1",
+        precio: 1
+    },
+    {
+        nombre: "Producto 2",
+        descripcion: "Descripción 2",
+        precio: 2
+    },
+    {
+        nombre: "Producto 3",
+        descripcion: "Descripción 3",
+        precio: 3
+    },
+    {
+        nombre: "Producto 4",
+        descripcion: "Descripción 4",
+        precio: 4
+    },
+    {
+        nombre: "Producto 5",
+        descripcion: "Descripción 5",
+        precio: 5
+    },
+    {
+        nombre: "Producto 6",
+        descripcion: "Descripción 6",
+        precio: 6
+    }
+];
+
+let peliculas = [
+    {
+        titulo: "Película Parte I",
+        genero: "Acción",
+        duracion: 120
+    },
+    {
+        titulo: "Película Parte II",
+        genero: "Acción",
+        duracion: 125
+    },
+    {
+        titulo: "Película Parte III",
+        genero: "Acción",
+        duracion: 115
+    },
+    {
+        titulo: "Película Parte IV",
+        genero: "Acción",
+        duracion: 185
+    },
+    {
+        titulo: "Otra película",
+        genero: "Comedia",
+        duracion: 105
+    },
+];
